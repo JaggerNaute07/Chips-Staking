98d255fe390722384ecc6e2fbf6a57b444a1ce4f
JaggerNaute07 <dewanggaputri7@gmail.com>
2025-06-26 16:34:18 +0800

    Delete env.js

--- env.js
+++
@@ -1,8 +0,0 @@
-window.env = {
-    FIREBASE_API_KEY: "AIzaSyAQSdCEBaCr-0u7PmCU0x319rjYOIXbCAw",
-    FIREBASE_AUTH_DOMAIN: "chips-staking.firebaseapp.com",
-    FIREBASE_PROJECT_ID: "chips-staking",
-    FIREBASE_STORAGE_BUCKET: "chips-staking.firebasestorage.app",
-    FIREBASE_MESSAGING_SENDER_ID: "271418482976",
-    FIREBASE_APP_ID: "1:271418482976:web:8a46614ac4f07512a87745"
-  };
