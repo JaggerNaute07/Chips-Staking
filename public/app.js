3e119b3e12301b5535d5dd48fd32ec5979b3ba21
JaggerNaute07 <dewanggaputri7@gmail.com>
2025-06-26 16:33:46 +0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1510,20 +1510,21 @@ let isNewPoolsHidden = false;
 let isCreatePoolHidden = false;
 let userInteractions = [];
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: window.env.FIREBASE_API_KEY,
   authDomain: window.env.FIREBASE_AUTH_DOMAIN,
   projectId: window.env.FIREBASE_PROJECT_ID,
   storageBucket: window.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: window.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: window.env.FIREBASE_APP_ID
-});
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 // Setelah initialize, baru akses Firestore
 const db = firebase.firestore();
 
-
-firebase.initializeApp(firebaseConfig);
 
 // Fungsi log interaksi ke Firestore
 async function logInteraction(action) {
