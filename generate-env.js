1e0e9edf5d4d28799d39e2af3546daa537b3b370
JaggerNaute07 <dewanggaputri7@gmail.com>
2025-06-26 19:12:53 +0800

    Update generate-env.js

--- generate-env.js
+++ generate-env.js
@@ -1,16 +1,14 @@
 const fs = require('fs');
-const path = require('path');
 
-const output = `
-window.env = {
-  FIREBASE_API_KEY: "${process.env.FIREBASE_API_KEY}",
-  FIREBASE_AUTH_DOMAIN: "${process.env.FIREBASE_AUTH_DOMAIN}",
-  FIREBASE_PROJECT_ID: "${process.env.FIREBASE_PROJECT_ID}",
-  FIREBASE_STORAGE_BUCKET: "${process.env.FIREBASE_STORAGE_BUCKET}",
-  FIREBASE_MESSAGING_SENDER_ID: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
-  FIREBASE_APP_ID: "${process.env.FIREBASE_APP_ID}"
+const env = {
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID
 };
-`;
 
-fs.writeFileSync(path.join(__dirname, 'public', 'env.js'), output);
-console.log("✅ env.js generated with Vercel environment variables.");
+const content = `window.env = ${JSON.stringify(env, null, 2)};`;
+fs.writeFileSync('public/env.js', content);
+console.log('✅ env.js berhasil dibuat dari Vercel environment variable');
