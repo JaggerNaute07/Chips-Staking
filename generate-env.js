8ee6dc52ccbc385aebee55368f43e225df3c9279
JaggerNaute07 <dewanggaputri7@gmail.com>
2025-06-26 19:17:56 +0800

    Update generate-env.js

--- generate-env.js
+++ generate-env.js
@@ -1,3 +1,4 @@
+// generate-env.js
 const fs = require('fs');
 
 const env = {
@@ -6,9 +7,10 @@ const env = {
   FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
   FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
   FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID
+  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
 };
 
 const content = `window.env = ${JSON.stringify(env, null, 2)};`;
+
 fs.writeFileSync('public/env.js', content);
-console.log('✅ env.js berhasil dibuat dari Vercel environment variable');
+console.log('✅ env.js generated in public/env.js');
